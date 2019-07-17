3bce0a37d16b63cef1cea8bfeb945803df6b1c82
IvanBall8 <44273358+IvanBall8@users.noreply.github.com>
2019-07-17 10:33:36 -0500

    Update TimeLapse.py

--- TimeLapse.py
+++ TimeLapse.py
@@ -27,7 +27,8 @@ while True:
 	elif hour>21 and hour<=5:
 		timeGood = 0
 	print "timeGood =", timeGood
-	
+	with picamera.PiCamera() as camera:
+		camera.close()
 	Reed1 = GPIO.input(PROX1)
 	if Reed1 == closed and state == open and timeGood == 1:
 		GPIO.output(LED, 1)
@@ -35,17 +36,26 @@ while True:
 		state = closed
 		time.sleep(1)
 		with picamera.PiCamera() as camera:
+			print "1"
 			camera.resolution = (2592, 1944)
+			print "2"
 			camera.vflip = True
+			print "3"
 			date = datetime.datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
+			print "4"
 			camera.start_preview()
+			print "5"
 			time.sleep(2)
+			print "6"
 			camera.capture("/media/exfat/TimeLapse6/"+ date +".jpg")
+			print "7"
 			print("Picture Taken")
+			camera.stop_preview()
 			time.sleep(20)
 			GPIO.output(LED, 0)
 	elif Reed1 == open and state == closed:
 		print("switch open")
 		state = open
+	
 	time.sleep(5)
   
