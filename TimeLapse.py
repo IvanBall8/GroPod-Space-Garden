e9b0095e59faee43860d2cbc7c7e7b10676eb046
IvanBall8 <44273358+IvanBall8@users.noreply.github.com>
2019-07-18 12:39:55 -0500

    Update TimeLapse.py

--- TimeLapse.py
+++ TimeLapse.py
@@ -2,7 +2,7 @@
 
 import time
 import datetime
-from picamera import PiCamera
+from picamera import PiCamera as camera
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
@@ -19,7 +19,7 @@ GPIO.setup(LED, GPIO.OUT, initial=GPIO.LOW)
 
 time.sleep(1)
 
-camera = PiCamera()
+#camera = PiCamera()
 
 while True:
 	hour = datetime.datetime.now().hour
