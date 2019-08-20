636ef14bcce6f8d8a96a9ce107b681b25a94ae1a
IvanBall8 <44273358+IvanBall8@users.noreply.github.com>
2019-08-20 10:47:30 -0500

    Update TimeLapse.py

--- TimeLapse.py
+++ TimeLapse.py
@@ -36,9 +36,10 @@ while True:
 		print "timeGood =", timeGood
 		time.sleep(1)
 		#date = datetime.datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
-		#os.system('fswebcam -r 2592x1944 -S 3 --jpeg 50 --set brightness=98% --save /media/exfat/TimeLapse6/%m_%d_%Y_%H_%M_%S".jpg'
+		os.system('fswebcam -r 2592x1944 -S 3 --jpeg 50 --set brightness=98% --delay 3 --save /media/exfat/TimeLapse6/%s.jpeg" %datetime.datetime.now().strftime("%m_%d_%Y_%H_%M_%S"))
 		#time.sleep(1)
 		#os.system('pkill fswebcam')
+		"""
 		try:
 			with picamera.PiCamera() as camera:
 				print "1"
@@ -64,6 +65,7 @@ while True:
 				GPIO.output(LED, 0)
 		except picamera.PiCameraMMALError:
 			print('Raised PiCameraMMALError')
+		"""
 	elif Reed1 == open and state == closed:
 		print("switch open")
 		state = open
