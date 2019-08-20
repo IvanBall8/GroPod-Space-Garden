82bf0657f392222d978fb000a59ee8472395b7a0
IvanBall8 <44273358+IvanBall8@users.noreply.github.com>
2019-08-20 17:27:51 -0500

    Update TimeLapse.py

--- TimeLapse.py
+++ TimeLapse.py
@@ -36,7 +36,7 @@ while True:
 		print "timeGood =", timeGood
 		time.sleep(1)
 		date = datetime.datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
-		os.system("fswebcam -r 2592x1944 -S 3 --jpeg 50 --set brightness=98% --delay 3 --save /media/exfat/TimeLapse6/"+ date +".jpg")
+		os.system("fswebcam -r 2592x1944 -S 3 --jpeg 50 --set brightness=50% --delay 3 --save /media/exfat/TimeLapse6/"+ date +".jpg")
 		#time.sleep(1)
 		#os.system('pkill fswebcam')
 		"""
